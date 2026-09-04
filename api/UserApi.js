701663c44df0a9464e8b73f4f4a5be15912f326b
PrajwalGpy <prajwalgpy@gmail.com>
2026-09-04 15:27:53 +0530

    api utility done

--- api/UserApi.js
+++ api/UserApi.js
@@ -1,21 +1,23 @@
-class UserApi{
-    constructor(request){
-        this.request = request
+export class UserApi {
+  constructor(request) {
+    this.request = request;
 
-        this.baseURL = process.env.API_URL;
-    }
+    this.baseURL = process.env.API_URL;
+  }
 
-    async createUser(userData){
-        return  await this.request.post(
-             `${this.baseURL}/api/users`,{
-                data:userData
-             }
-        )
-    }
+  async createUser(userData) {
+    return await this.request.post(`${this.baseURL}/api/users`, {
+      data: userData,
+    });
+  }
 
-    async getUser(userId){
-        return await this.request.get(
-            `${this.baseURL}/api/users/${userId}`
-        )
-    }
+  async getUser(userId) {
+    return await this.request.get(`${this.baseURL}/api/users/${userId}`);
+  }
+
+  async updateUser(userId, userData) {
+    return await this.request.put(`${this.baseURL}/api/users/${userId}`, {
+      data: userData,
+    });
+  }
 }
