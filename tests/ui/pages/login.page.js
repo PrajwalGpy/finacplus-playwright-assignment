f81b34e6ff6a91bd9551f6afc22dc55a22984816
PrajwalGpy <prajwalgpy@gmail.com>
2026-09-04 11:16:19 +0530

    setup

--- tests/ui/pages/login.page.js
+++ tests/ui/pages/login.page.js
@@ -1,5 +1,10 @@
 class LoginPage{
     constructor(page){
         this.page = page
+
+        this.usernameInput = page.getByPlaceholder("UserName")
+        this.passwordInput = page.getByPlaceholder("Password")
+        this.loginButton = page.getByRole('button',{name:'Login'})
+        this.logoutButton = page.getByRole('Button',{name:'Logout'})
     }
 }
