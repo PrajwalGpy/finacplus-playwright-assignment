18b49c9fb66c2a8b2818c4b567eb55a48346aa9a
PrajwalGpy <prajwalgpy@gmail.com>
2026-09-04 11:20:02 +0530

    goto & login done

--- tests/ui/pages/login.page.js
+++ tests/ui/pages/login.page.js
@@ -7,4 +7,14 @@ class LoginPage{
         this.loginButton = page.getByRole('button',{name:'Login'})
         this.logoutButton = page.getByRole('Button',{name:'Logout'})
     }
+
+    async goto(){
+        await this.page.goto('/login')
+    }
+
+    async login(username,password){
+        await this.usernameInput.fill(username)
+        await this.passwordInput.fill(password)
+        await this.loginButton.click()
+    }
 }
