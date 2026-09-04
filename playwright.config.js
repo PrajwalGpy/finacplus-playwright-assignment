c207a431ef04c42b1760c27942ca7a521ee36531
PrajwalGpy <prajwalgpy@gmail.com>
2026-09-04 11:06:46 +0530

    project structure created

--- playwright.config.js
+++ playwright.config.js
@@ -30,7 +30,11 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    baseURL: 'https://demoqa.com',
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'on-first-retry'
   },
 
   /* Configure projects for major browsers */
